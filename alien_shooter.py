2b07da85755d48e0359c8e50ebe9be1b418d2ae0
Chris <45928077+infosechris@users.noreply.github.com>
2020-04-04 23:19:54 -0400

    Update alien_shooter.py

--- alien_shooter.py
+++ alien_shooter.py
@@ -1,10 +1,20 @@
 import pygame
 
+#Variables needed for Gamepad
 bg_color = (240, 245, 242)
 txt_color = (0, 0, 0)
 sc_width = 480
 sc_height = 640	
 
+#Initialized Game, Gamepad size, BG color, Game Title 
 pygame.init()
 gamepad = pygame.display.set_mode((sc_width, sc_height))
+gamepad.fill(bg_color)
 pygame.display.set_caption('Alien Shooter')
+
+#Makes the game screen stay on for now
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
